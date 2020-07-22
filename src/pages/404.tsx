8ffafd3e1feb60f59412349ed5dd9b7428d1548d
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-22 18:34:06 -0300

    Small changes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,17 @@
-import React from "react"
+import React from 'react'
 
-import { Layout, SEO } from "../components"
+import { Layout, SEO } from '../components'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <React.Fragment>
+      <Layout>
+        <SEO title='404: Not found' />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    </React.Fragment>
+  )
+}
 
 export default NotFoundPage
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,15 +1,19 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import { Layout, SEO } from "../components"
+import { Layout, SEO } from '../components'
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  return (
+    <React.Fragment>
+      <Layout>
+        <SEO title='Page two' />
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+        <Link to='/'>Go back to the homepage</Link>
+      </Layout>
+    </React.Fragment>
+  )
+}
 
 export default SecondPage
