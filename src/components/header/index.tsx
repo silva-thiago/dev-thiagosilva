14c78c240280fd89a202ed09a6c991e406d94053
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-22 21:24:43 -0300

    New navigation bar developed

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,51 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-const Header = ({ siteTitle }) => (
-  <header style={{ background: `rebeccapurple`, marginBottom: `1.45rem` }}>
-    <div style={{ margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem` }}>
-      <h1 style={{ margin: `0` }}>
-        <Link to="/" style={{ color: `white`, textDecoration: `none` }}>
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const links = [
+  { href: 'https://github.com/silva-thiago', label: 'GitHub', target: '_blank' },
+  { href: 'https://www.linkedin.com/in/tjlsilva/', label: 'LinkedIn', target: '_blank' },
+]
+
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "logotype.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 30) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <React.Fragment>
+      <header>
+        {/* Fixed navbar */}
+        <nav className='nav fixed-top'>
+          <div className='d-flex justify-content-between items-center list-unstyled mb-0 mr-auto'>
+            <Link className='navbar-brand' to='/'>
+              <Img className='logotype' fluid={data.placeholderImage.childImageSharp.fluid} alt='Logotipo' loading='auto' />
+              <span className='text-dark'>{siteTitle} Developer_</span>
+            </Link>
+          </div>
+          <ul className='list-unstyled mb-0'>
+            {links.map(({ href, label, target }) => (
+              <li className='btn-group mx-2' key={`${href}${label}${target}`}>
+                <a className='btn btn-sm btn-outline-dark rounded-pill' href={href} target={target} role='button'>
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </React.Fragment>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
