bebfd042e2f87d2af44cb08fe64fea62aa69b621
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-22 21:19:50 -0300

    New index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,48 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import { Layout, Image, SEO } from "../components"
+import { Layout, Image, SEO } from '../components'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const links = [
+  { href: 'https://t.me/silvathiago', label: 't.me/silvathiago', target: '_blank' },
+]
+
+const IndexPage = () => {
+  return (
+    <React.Fragment>
+      <Layout>
+        <SEO title='Home' />
+        <section className='text-center h-center-alignment'>
+          <div className=''>
+            <h2>Hi, I am learning fullstack development</h2>
+            <p>I design and code beautifully simple things.</p>
+            <p>thiago.silva@imd.ufrn.br</p>
+            <ul className='list-unstyled'>
+              {links.map(({ href, label, target }) => (
+                <li key={`${href}${label}${target}`}>
+                  <a className='text-decoration-none' href={href} target={target}>
+                    {label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className=''>
+            <p className=''>Living, learning &amp; leveling up one day at a time.</p>
+          </div>
+        </section>
+        <div style={{ maxWidth: `300px`, margin: `auto` }}>
+          <Image />
+        </div>
+        <section className='text-center h-center-alignment'>
+          <div>
+            <Link to='/page-2'>Go to page 2</Link><br />
+            <Link to='/using-typescript'>Go to Using TypeScript</Link>
+          </div>
+        </section>
+      </Layout>
+    </React.Fragment>
+  )
+}
 
 export default IndexPage
