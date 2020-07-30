4e9c45da6a0da623054dc7f84d39098b91fca8fe
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-29 22:09:13 -0300

    Melhorias de desempenho e segurança (links ext.)

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 
 const Footer = ({ siteAuthor }) => {
+  const ref = useRef('noopener noreferrer')
+
   return (
     <React.Fragment>
       <footer className='footer py-2'>
         <div className='container'>
           <span className='text-muted'>
-            © {new Date().getFullYear()} <a className='text-decoration-none' href='https://github.com/silva-thiago' target='_blank'>{siteAuthor}</a><br />
+            © {new Date().getFullYear()} <a className='text-decoration-none' href='https://github.com/silva-thiago' target='_blank' ref={ref}>{siteAuthor}</a><br />
           </span>
           <span className='text-muted'>
             <small>
               This page was built with GatsbyJS. The source code can be found by clicking on the link below:<br />
-              <a className='text-decoration-none' href='https://github.com/silva-thiago/dev-thiagosilva' target='_blank'>github.com/silva-thiago/dev-thiagosilva</a>
+              <a className='text-decoration-none' href='https://github.com/silva-thiago/dev-thiagosilva' target='_blank' ref={ref}>github.com/silva-thiago/dev-thiagosilva</a>
             </small>
           </span>
         </div>
