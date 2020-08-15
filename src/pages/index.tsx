c72811359a3445b2aa7520372d2da04a76f376aa
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-15 17:41:32 -0300

    Remoção de classes Bootstrap + correção de bug em link externo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,26 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 
-import { Layout, Section, Image, SEO, Repos } from '../components'
+import { Landing, Section, Image, SEO, Repos } from '../components'
 
 const links = [
   { href: 'https://t.me/silvathiago', label: 't.me/silvathiago', target: '_blank' },
 ]
 
 const IndexPage = () => {
-  const ref = useRef('noopener noreferrer')
-
   return (
     <React.Fragment>
-      <Layout>
+      <Landing>
         <SEO title='Home' />
         <Section>
           <div className='text-center'>
             <h2>Hi, I am learning fullstack development</h2>
             <p>I design and code beautifully simple things.</p>
             <p>thiago.silva@imd.ufrn.br</p>
-            <ul className='list-unstyled'>
+            <ul>
               {links.map(({ href, label, target }) => (
                 <li key={`${href}${label}${target}`}>
-                  <a className='font-weight-bold text-decoration-none' href={href} target={target} ref={ref}>
+                  <a href={href} target={target} rel='noopener noreferrer'>
                     {label}
                   </a>
                 </li>
@@ -30,22 +28,22 @@ const IndexPage = () => {
             </ul>
           </div>
           <div className='text-center'>
-            <p className='my-4'>Living, learning &amp; leveling up one day at a time.</p>
+            <p>Living, learning &amp; leveling up one day at a time.</p>
           </div>
         </Section>
-        <div style={{ maxWidth: `300px`, margin: `auto` }}>
+        {/* <div style={{ maxWidth: `300px`, margin: `auto` }}>
           <Image />
-        </div>
-        <Section>
+        </div> */}
+        {/* <Section>
           <div className='text-center'>
-            <Link className='text-decoration-none' to='/page-2'>Go to page 2</Link><br />
-            <Link className='text-decoration-none' to='/using-typescript'>Go to Using TypeScript</Link>
+            <Link to='/page-2'>Go to page 2</Link><br />
+            <Link to='/using-typescript'>Go to Using TypeScript</Link>
           </div>
-        </Section>
+        </Section> */}
         <Section>
           <Repos />
         </Section>
-      </Layout>
+      </Landing>
     </React.Fragment>
   )
 }
