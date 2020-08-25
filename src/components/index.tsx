3917bc520a5d364405891faaa901eb0b0e70094f
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-24 22:49:54 -0300

    Arquivos renomeados, realocados e/ou deletados

--- src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Header from './Header'
-import Landing from './Landing'
-import Footer from './Footer'
-import Section from './Section'
-import Repos from './Repos'
-import SEO from './SEO'
-import Image from './Image'
-
-export { Header, Landing, Footer, Section, Repos, SEO, Image }
