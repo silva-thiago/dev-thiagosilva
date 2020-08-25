e088de3c842280525a5ade7c1eb1e6dc7eeab12f
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-24 23:02:10 -0300

    Novo cabeçalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,62 +1,32 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-
-interface IProps {
-  siteTitle: string,
-}
 
 const links = [
   { href: 'https://github.com/silva-thiago', label: 'GitHub', target: '_blank' },
   { href: 'https://www.linkedin.com/in/tjlsilva/', label: 'LinkedIn', target: '_blank' },
 ]
 
-const Header:React.FC<IProps> = ({ siteTitle }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "logotype.png"}) {
-        childImageSharp {
-          fluid(maxWidth: 30) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
+const Header = () => {
   return (
     <React.Fragment>
       <header>
-        {/* Fixed navbar */}
-        <nav className='nav navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-sm'>
-          <div className='d-flex justify-content-between items-center mb-0'>
-            <Link className='navbar-brand' to='/'>
-              <Img className='logo' fluid={data.placeholderImage.childImageSharp.fluid} alt='Logotipo' loading='auto' />
-              <span className='text-dark'>{siteTitle} Developer_</span>
-            </Link>
-          </div>
-          <ul className='list-unstyled mb-0'>
+        <div className='container'>
+          <h1>Hi, I am learning fullstack development</h1>
+          <p>I design and code beautifully simple things.</p>
+          <p>thiago.silva@imd.ufrn.br</p>
+          <ul>
             {links.map(({ href, label, target }) => (
-              <li className='btn-group mx-2' key={`${href}${label}${target}`}>
-                <a className='btn btn-outline-dark rounded-pill' href={href} target={target} rel='noopener noreferrer' role='button'>
+              <li key={`${href}${label}${target}`}>
+                <a href={href} target={target} rel='noopener noreferrer'>
                   {label}
                 </a>
               </li>
             ))}
           </ul>
-        </nav>
+          <p>Living, learning &amp; leveling up one day at a time.</p>
+        </div>
       </header>
     </React.Fragment>
   )
-}
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
