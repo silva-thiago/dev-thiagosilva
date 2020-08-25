d167565f55b12619685209d118827d3dd3cc245b
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-24 23:00:25 -0300

    Arquivo de declaração de tipos

---
+++ gatsby-app.d.ts
@@ -0,0 +1,6 @@
+/// <reference types='react' />
+/// <reference types='react-dom' />
+/// <reference types='react-helmet' />
+/// <reference types='react-scripts' />
+/// <reference types='node' />
+/// <reference types='typography' />
